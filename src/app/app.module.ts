1ecf372957f384ea603df50bb346ba2fe3a69214
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-07-10 22:44:16 +0600

    header footer and main-container component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContainerComponent } from './main-container/main-container.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule
